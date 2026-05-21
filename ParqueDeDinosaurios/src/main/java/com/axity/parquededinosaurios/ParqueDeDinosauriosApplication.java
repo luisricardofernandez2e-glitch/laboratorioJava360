d6a7d51acd65fd62a2fbc6d8cefb442a989b2cbb
LuisR <luisricardofernandez.2e@gmail.com>
2026-05-20 19:25:56 -0600

    feat: Creacion proyecto Maven

---
+++ ParqueDeDinosaurios/src/main/java/com/axity/parquededinosaurios/ParqueDeDinosauriosApplication.java
@@ -0,0 +1,13 @@
+package com.axity.parquededinosaurios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParqueDeDinosauriosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParqueDeDinosauriosApplication.class, args);
+    }
+
+}
---
+++ ParqueDeDinosaurios/src/test/java/com/axity/parquededinosaurios/ParqueDeDinosauriosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axity.parquededinosaurios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParqueDeDinosauriosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
